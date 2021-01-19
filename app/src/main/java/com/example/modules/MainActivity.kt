a7fe348c16171d9523b9f24ab3c9e33a2c81960d
Arif <cse.ariful@gmail.com>
2021-01-19 17:45:18 +0600

    removed billing module

--- app/src/main/java/com/example/modules/MainActivity.kt
+++ app/src/main/java/com/example/modules/MainActivity.kt
@@ -2,10 +2,11 @@ package com.example.modules
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.FrameLayout
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(FrameLayout(this))
     }
 }
